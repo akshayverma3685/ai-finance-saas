51898394a26d67edab0273ccb8484e0e703531fd
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 15:29:47 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,26 @@
 module.exports = {
   root: true,
-  env: { 
-    node: true, 
-    browser: true, 
-    es2022: true 
+  env: {
+    node: true,
+    browser: true,
+    es2022: true
   },
   extends: [
-    "eslint:recommended", 
+    "eslint:recommended",
     "plugin:react/recommended"
   ],
-  parserOptions: { 
-    ecmaVersion: "latest", 
-    sourceType: "module" 
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
   plugins: ["react"],
   rules: {
-    // agar variable declare hai but use nahi hua to warning aayegi
     "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-    // React ke prop-types wali warning off kar dena (aap TypeScript use nahi kar rahe ho)
     "react/prop-types": "off"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 };
