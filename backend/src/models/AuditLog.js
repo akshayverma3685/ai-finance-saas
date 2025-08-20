6376a99b1a2324bf996b6fc74300549c0e4401da
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 12:10:20 +0530

    Update AuditLog.js

--- backend/src/models/AuditLog.js
+++ backend/src/models/AuditLog.js
@@ -1,1 +1,7 @@
-
+import mongoose from "mongoose";
+const AuditLogSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  action: String,
+  meta: Object
+}, { timestamps: true });
+export default mongoose.model("AuditLog", AuditLogSchema);
