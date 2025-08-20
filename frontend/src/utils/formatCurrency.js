0feb907513c5cd243b660f6a79702a7ebf850174
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 12:48:37 +0530

    Update formatCurrency.js

--- frontend/src/utils/formatCurrency.js
+++ frontend/src/utils/formatCurrency.js
@@ -1,1 +1,7 @@
-
+// frontend/src/utils/formatCurrency.js
+export default function formatCurrency(amount, currency = "USD") {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(amount);
+}
