373300b9b89c73a46720e8d7e9828e58b8ddaafc
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 12:13:29 +0530

    Update expense.controller.js

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -1,1 +1,25 @@
+import Expense from "../models/Expense.js";
 
+export async function createExpense(req, res) {
+  const exp = await Expense.create({ ...req.body, userId: req.user._id });
+  res.json(exp);
+}
+export async function listExpenses(req, res) {
+  const { from, to } = req.query;
+  const q = { userId: req.user._id };
+  if (from || to) q.date = {};
+  if (from) q.date.$gte = new Date(from);
+  if (to) q.date.$lte = new Date(to);
+  const items = await Expense.find(q).sort({ date: -1 });
+  res.json(items);
+}
+export async function updateExpense(req, res) {
+  const { id } = req.params;
+  const exp = await Expense.findOneAndUpdate({ _id: id, userId: req.user._id }, req.body, { new: true });
+  res.json(exp);
+}
+export async function deleteExpense(req, res) {
+  const { id } = req.params;
+  await Expense.deleteOne({ _id: id, userId: req.user._id });
+  res.json({ success: true });
+}
