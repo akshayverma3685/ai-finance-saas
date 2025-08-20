8eff432b2a7dd8365535e85c4c318c5e589ad81a
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 12:28:22 +0530

    Create Billing.jsx

---
+++ frontend/src/pages/Billing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import api from "../utils/api.js";
+export default function Billing(){
+  const openPortal = async ()=>{
+    const { data } = await api.post("/stripe/create-portal-session", {});
+    window.location.href = data.url;
+  };
+  return (
+    <div>
+      <h2>Billing</h2>
+      <button onClick={openPortal}>Open Billing Portal</button>
+    </div>
+  );
+}
