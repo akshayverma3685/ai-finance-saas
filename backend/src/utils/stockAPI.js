92b6b8ae02374bdd8638f360925b93773de92950
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 13:08:42 +0530

    Update stockAPI.js

--- backend/src/utils/stockAPI.js
+++ backend/src/utils/stockAPI.js
@@ -1,1 +1,15 @@
+// backend/src/utils/stockApi.js
+import axios from "axios";
 
+const API_URL = "https://finnhub.io/api/v1/quote";
+const API_KEY = process.env.STOCK_API_KEY;
+
+export const getStockPrice = async (symbol) => {
+  try {
+    const res = await axios.get(`${API_URL}?symbol=${symbol}&token=${API_KEY}`);
+    return res.data.c;
+  } catch (err) {
+    console.error("Stock API error", err);
+    return null;
+  }
+};
