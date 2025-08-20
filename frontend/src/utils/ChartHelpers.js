e2f1d3337b32ed8e32ca7c65ffb08b2784412656
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-20 13:13:05 +0530

    Update ChartHelpers.js

--- frontend/src/utils/ChartHelpers.js
+++ frontend/src/utils/ChartHelpers.js
@@ -1,1 +1,12 @@
+// frontend/src/utils/chartHelper.js
+export const prepareExpenseChartData = (expenses) => {
+  const grouped = expenses.reduce((acc, exp) => {
+    acc[exp.category] = (acc[exp.category] || 0) + exp.amount;
+    return acc;
+  }, {});
 
+  return Object.entries(grouped).map(([category, amount]) => ({
+    name: category,
+    value: amount
+  }));
+};
