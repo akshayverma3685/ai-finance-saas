7d7b9fffe5df5c15879967b36eae716437bb3db8
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 04:14:18 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-// main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./App";
+import "./index.css"; // Tailwind global styles
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
