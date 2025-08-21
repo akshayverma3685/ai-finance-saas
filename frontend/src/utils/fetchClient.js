ba6b96eaf500032277c3b2b17a2fb7e6054ebee5
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 05:13:44 +0530

    Update fetchClient.js

--- frontend/src/utils/fetchClient.js
+++ frontend/src/utils/fetchClient.js
@@ -1,7 +1,14 @@
-export async function fetchClient(url, options = {}) {
-  const res = await fetch(url, {
-    headers: { "Content-Type": "application/json" },
-    ...options,
-  });
-  return res.json();
-}
+import axios from "axios";
+import { getToken } from "./auth";
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+});
+
+client.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default client;
