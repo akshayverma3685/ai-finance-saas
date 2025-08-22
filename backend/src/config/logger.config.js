66da6a38b996796d66e122d49f90cd68688906e0
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 22:37:41 +0530

    Update logger.config.js

--- backend/src/config/logger.config.js
+++ backend/src/config/logger.config.js
@@ -1,16 +1,3 @@
-import winston from "winston";
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
-  ],
-});
-
-export default logger;
+import pino from 'pino'
+const logger = pino({ transport: { target: 'pino-pretty' } })
+export default logger
