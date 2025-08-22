e27e1d4a86b89d2106026b9eee431415313648fb
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 12:43:18 +0530

    Update and rename logger.js to logger.config.js

--- backend/src/config/logger.js
+++
@@ -1,4 +0,0 @@
-// backend/src/utils/logger.js
-export const logInfo = (msg) => console.log(`ℹ️ [INFO]: ${msg}`);
-export const logError = (msg, err) => console.error(`❌ [ERROR]: ${msg}`, err);
-export const logWarn = (msg) => console.warn(`⚠️ [WARN]: ${msg}`);
