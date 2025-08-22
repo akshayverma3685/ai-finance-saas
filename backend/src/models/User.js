660e3316dafea21ce9c12e1525eea6ae2c58d490
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 00:47:15 +0530

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose'
+
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true, lowercase: true, index: true },
     password: { type: String, required: true },
-    isPro: { type: Boolean, default: false }
+    isPro: { type: Boolean, default: false },
+
+    // 👇 Add this new field
+    role: { 
+      type: String, 
+      enum: ["user", "admin"], 
+      default: "user" 
+    }
   },
   { timestamps: true }
 )
+
 export default mongoose.model('User', UserSchema)
