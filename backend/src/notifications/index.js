3b1eaea0618ccd3400028405e6f052fbf42d8b45
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 12:59:17 +0530

    Update index.js

--- backend/src/notifications/index.js
+++ backend/src/notifications/index.js
@@ -1,1 +1,9 @@
+const routes = require("./notification.routes");
+const controller = require("./notification.controller");
+const service = require("./notification.service");
 
+module.exports = {
+  routes,
+  controller,
+  service,
+};
