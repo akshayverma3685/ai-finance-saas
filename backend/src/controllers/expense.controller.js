0b87dfa2123f0459b2b98db8974e332ece3e0e4c
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 22:59:17 +0530

    Update expense.controller.js

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -1,25 +1,6 @@
-import Expense from "../models/Expense.js";
+import { listExpenses, createExpense, deleteExpense } from '../services/expense.service.js'
+import { ok, created } from '../utils/apiResponse.js'
 
-export async function createExpense(req, res) {
-  const exp = await Expense.create({ ...req.body, userId: req.user._id });
-  res.json(exp);
-}
-export async function listExpenses(req, res) {
-  const { from, to } = req.query;
-  const q = { userId: req.user._id };
-  if (from || to) q.date = {};
-  if (from) q.date.$gte = new Date(from);
-  if (to) q.date.$lte = new Date(to);
-  const items = await Expense.find(q).sort({ date: -1 });
-  res.json(items);
-}
-export async function updateExpense(req, res) {
-  const { id } = req.params;
-  const exp = await Expense.findOneAndUpdate({ _id: id, userId: req.user._id }, req.body, { new: true });
-  res.json(exp);
-}
-export async function deleteExpense(req, res) {
-  const { id } = req.params;
-  await Expense.deleteOne({ _id: id, userId: req.user._id });
-  res.json({ success: true });
-}
+export const listCtrl = async (req, res, next) => { try { return ok(res, { items: await listExpenses(req.user._id) }) } catch (e) { next(e) } }
+export const createCtrl = async (req, res, next) => { try { return created(res, { item: await createExpense(req.user._id, req.body) }) } catch (e) { next(e) } }
+export const deleteCtrl = async (req, res, next) => { try { await deleteExpense(req.user._id, req.params.id); return ok(res, {}) } catch (e) { next(e) } }
