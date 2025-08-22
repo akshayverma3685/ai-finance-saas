257fe29671b595da9bab9e01626fcfd2e1c7b31d
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 23:58:14 +0530

    Update expense.controller.js

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -1,6 +1,54 @@
-import { listExpenses, createExpense, deleteExpense } from '../services/expense.service.js'
+import {
+  createExpense,
+  getExpenses,
+  getExpenseById,
+  updateExpense,
+  deleteExpense
+} from '../services/expense.service.js'
 import { ok, created } from '../utils/apiResponse.js'
 
-export const listCtrl = async (req, res, next) => { try { return ok(res, { items: await listExpenses(req.user._id) }) } catch (e) { next(e) } }
-export const createCtrl = async (req, res, next) => { try { return created(res, { item: await createExpense(req.user._id, req.body) }) } catch (e) { next(e) } }
-export const deleteCtrl = async (req, res, next) => { try { await deleteExpense(req.user._id, req.params.id); return ok(res, {}) } catch (e) { next(e) } }
+export const listExpensesCtrl = async (req, res, next) => {
+  try {
+    const expenses = await getExpenses(req.user._id)
+    return ok(res, { items: expenses })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const createExpenseCtrl = async (req, res, next) => {
+  try {
+    const exp = await createExpense(req.user._id, req.body)
+    return created(res, { item: exp })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getExpenseCtrl = async (req, res, next) => {
+  try {
+    const exp = await getExpenseById(req.user._id, req.params.id)
+    if (!exp) return res.status(404).json({ error: 'Not found' })
+    return ok(res, { item: exp })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const updateExpenseCtrl = async (req, res, next) => {
+  try {
+    const exp = await updateExpense(req.user._id, req.params.id, req.body)
+    return ok(res, { item: exp })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const deleteExpenseCtrl = async (req, res, next) => {
+  try {
+    await deleteExpense(req.user._id, req.params.id)
+    return ok(res, { success: true })
+  } catch (err) {
+    next(err)
+  }
+}
