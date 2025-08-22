c72213038eb2cf131671fe898978a5910aa366fd
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 00:30:16 +0530

    Update notification.controller.js

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,12 +1,43 @@
-import { listNotifications, createNotification, markAsRead } from '../services/notification.service.js'
-import { ok, created } from '../utils/apiResponse.js'
+import {
+  createNotification,
+  getNotifications,
+  markAsRead,
+  deleteNotification,
+} from "../services/notification.service.js"
+import { ok, created } from "../utils/apiResponse.js"
 
-export const listCtrl = async (req, res, next) => {
-  try { return ok(res, { items: await listNotifications(req.user._id) }) } catch (e) { next(e) }
+export const listNotificationsCtrl = async (req, res, next) => {
+  try {
+    const items = await getNotifications(req.user._id)
+    return ok(res, { items })
+  } catch (err) {
+    next(err)
+  }
 }
-export const createCtrl = async (req, res, next) => {
-  try { return created(res, { item: await createNotification(req.user._id, req.body) }) } catch (e) { next(e) }
+
+export const createNotificationCtrl = async (req, res, next) => {
+  try {
+    const item = await createNotification(req.user._id, req.body)
+    return created(res, { item })
+  } catch (err) {
+    next(err)
+  }
 }
+
 export const markReadCtrl = async (req, res, next) => {
-  try { return ok(res, { item: await markAsRead(req.user._id, req.params.id) }) } catch (e) { next(e) }
+  try {
+    const item = await markAsRead(req.user._id, req.params.id)
+    return ok(res, { item })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const deleteNotificationCtrl = async (req, res, next) => {
+  try {
+    await deleteNotification(req.user._id, req.params.id)
+    return ok(res, { success: true })
+  } catch (err) {
+    next(err)
+  }
 }
