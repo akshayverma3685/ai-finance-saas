543972606ba0eca018a64b0c4dd04547422906fd
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 23:57:06 +0530

    Update expense.service.js

--- backend/src/services/expense.service.js
+++ backend/src/services/expense.service.js
@@ -1,4 +1,40 @@
 import Expense from '../models/Expense.js'
-export const listExpenses = async (userId) => Expense.find({ user: userId }).sort({ date: -1 })
-export const createExpense = async (userId, payload) => Expense.create({ ...payload, user: userId })
-export const deleteExpense = async (userId, id) => Expense.findOneAndDelete({ _id: id, user: userId })
+
+/**
+ * Create a new expense
+ */
+export const createExpense = async (userId, data) => {
+  return await Expense.create({ ...data, user: userId })
+}
+
+/**
+ * Get all expenses of logged-in user
+ */
+export const getExpenses = async (userId) => {
+  return await Expense.find({ user: userId }).sort({ date: -1 })
+}
+
+/**
+ * Get single expense
+ */
+export const getExpenseById = async (userId, id) => {
+  return await Expense.findOne({ _id: id, user: userId })
+}
+
+/**
+ * Update expense
+ */
+export const updateExpense = async (userId, id, data) => {
+  return await Expense.findOneAndUpdate(
+    { _id: id, user: userId },
+    data,
+    { new: true }
+  )
+}
+
+/**
+ * Delete expense
+ */
+export const deleteExpense = async (userId, id) => {
+  return await Expense.findOneAndDelete({ _id: id, user: userId })
+}
