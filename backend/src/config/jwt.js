2865a67f8acba50da6f8b82948350251785989af
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 12:42:26 +0530

    Update and rename jwt.js to jwt.config.js

--- backend/src/config/jwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-export function signJwt(payload, opts = {}) {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d", ...opts });
-}
-export function verifyJwt(token) {
-  return jwt.verify(token, process.env.JWT_SECRET);
-}
