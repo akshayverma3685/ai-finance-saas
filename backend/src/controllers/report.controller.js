94abded7edc47a022f877a46d62d9ee866914e21
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 00:03:15 +0530

    Update report.controller.js

--- backend/src/controllers/report.controller.js
+++ backend/src/controllers/report.controller.js
@@ -1,5 +1,21 @@
-import { generateMonthlyReport } from '../services/report.service.js'
+import { generateReport, listReports } from '../services/report.service.js'
 import { ok } from '../utils/apiResponse.js'
-export const generateCtrl = async (req, res, next) => {
-  try { return ok(res, { report: await generateMonthlyReport(req.user._id, req.params.month) }) } catch (e) { next(e) }
+
+export const generateReportCtrl = async (req, res, next) => {
+  try {
+    const { month } = req.params
+    const report = await generateReport(req.user._id, month)
+    return ok(res, { report })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const listReportsCtrl = async (req, res, next) => {
+  try {
+    const reports = await listReports(req.user._id)
+    return ok(res, { items: reports })
+  } catch (err) {
+    next(err)
+  }
 }
