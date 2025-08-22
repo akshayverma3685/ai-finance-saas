47f2726cec7d2be793188e23f8820e47847ed60a
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 00:30:56 +0530

    Update notifications.routes.js

--- backend/src/routes/notifications.routes.js
+++ backend/src/routes/notifications.routes.js
@@ -1,10 +1,25 @@
-import { Router } from 'express'
-import { auth } from '../middlewares/auth.middleware.js'
-import { listCtrl, createCtrl, markReadCtrl } from '../controllers/notification.controller.js'
+import { Router } from "express"
+import { auth } from "../middlewares/auth.middleware.js"
+import {
+  listNotificationsCtrl,
+  createNotificationCtrl,
+  markReadCtrl,
+  deleteNotificationCtrl,
+} from "../controllers/notification.controller.js"
 
 const r = Router()
 r.use(auth)
-r.get('/', listCtrl)
-r.post('/', createCtrl)
-r.patch('/:id/read', markReadCtrl)
+
+// GET all notifications
+r.get("/", listNotificationsCtrl)
+
+// POST create new notification
+r.post("/", createNotificationCtrl)
+
+// PATCH mark as read
+r.patch("/:id/read", markReadCtrl)
+
+// DELETE notification
+r.delete("/:id", deleteNotificationCtrl)
+
 export default r
