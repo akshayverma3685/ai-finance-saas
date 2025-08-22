3a50f2b663a07967aefdde02bfff81ad7706b9bc
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 23:02:34 +0530

    Update payment.controller.js

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,1 +1,8 @@
-
+import { createOrder, markPaid } from '../services/payment.service.js'
+import { ok, created } from '../utils/apiResponse.js'
+export const createOrderCtrl = async (req, res, next) => {
+  try { const out = await createOrder(req.user._id, req.body.amount); return created(res, out, 'Order created') } catch (e) { next(e) }
+}
+export const markPaidCtrl = async (req, res, next) => {
+  try { return ok(res, { payment: await markPaid(req.body) }, 'Payment updated') } catch (e) { next(e) }
+}
