a43d78f3120b1657ab92345ef976a2d7feeee02c
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-22 13:16:06 +0530

    Create index.js

---
+++ backend/src/utils/index.js
@@ -0,0 +1,13 @@
+// src/utils/index.js
+
+module.exports = {
+  currencyConverter: require("./currencyConverter"),
+  dateUtils: require("./dateUtils"),
+  errorHandler: require("./errorHandler"),
+  generateReport: require("./generateReport"),
+  logger: require("./logger"),
+  metrics: require("./metrics"),
+  ocrProcessor: require("./ocrProcessor"),
+  stockAPI: require("./stockAPI"),
+  validator: require("./validater"), // filename 'validater.js' hai
+};
