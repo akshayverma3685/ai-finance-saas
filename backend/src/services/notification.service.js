0f32c2ab5fb237981e4c5b5548f7aa35f6447c83
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 00:29:13 +0530

    Update notification.service.js

--- backend/src/services/notification.service.js
+++ backend/src/services/notification.service.js
@@ -1,10 +1,33 @@
-import Notification from '../models/Notification.js'
+import Notification from "../models/Notification.js"
 
-export const listNotifications = async (userId) =>
-  Notification.find({ user: userId }).sort({ createdAt: -1 })
+/**
+ * Create a new notification
+ */
+export const createNotification = async (userId, data) => {
+  return await Notification.create({ ...data, user: userId })
+}
 
-export const createNotification = async (userId, payload) =>
-  Notification.create({ ...payload, user: userId })
+/**
+ * Get all notifications for user
+ */
+export const getNotifications = async (userId) => {
+  return await Notification.find({ user: userId }).sort({ createdAt: -1 })
+}
 
-export const markAsRead = async (userId, id) =>
-  Notification.findOneAndUpdate({ _id: id, user: userId }, { read: true }, { new: true })
+/**
+ * Mark notification as read
+ */
+export const markAsRead = async (userId, id) => {
+  return await Notification.findOneAndUpdate(
+    { _id: id, user: userId },
+    { isRead: true },
+    { new: true }
+  )
+}
+
+/**
+ * Delete notification
+ */
+export const deleteNotification = async (userId, id) => {
+  return await Notification.findOneAndDelete({ _id: id, user: userId })
+}
