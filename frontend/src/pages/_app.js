129aa455fafa29d17ddf6a688e623c62b71d7114
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 11:02:55 +0530

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,10 +1,6 @@
-// src/pages/_app.js
-import "../styles/globals.css"
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-// Tailwind CSS import
+// src/pages/_app.jsx
+import "@/styles/globals.css";
+import Script from "next/script";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -14,7 +10,6 @@ import { AuthProvider } from "@/context/AuthContext";
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  // Scroll to top on route change
   useEffect(() => {
     const handleRouteChange = () => {
       window.scrollTo(0, 0);
@@ -27,6 +22,11 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <AuthProvider>
+      {/* Razorpay checkout script */}
+      <Script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        strategy="afterInteractive"
+      />
       <Component {...pageProps} />
     </AuthProvider>
   );
