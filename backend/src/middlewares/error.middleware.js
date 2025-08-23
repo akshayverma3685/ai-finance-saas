3eee62ef9e1c4915beeebeecb88132c3ab80c307
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 22:10:18 +0530

    Update error.middleware.js

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,5 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
   console.error('Error:', err.message)
+
   res.status(err.status || 500).json({
     success: false,
     message: err.message || 'Internal Server Error',
