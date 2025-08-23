ffdb9419514bf5e78549fbd4483e89d66812013c
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 16:47:42 +0530

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,27 @@
+import AuditLog from "./AuditLog.js";
+import Billing from "./Billing.js";
+import Expense from "./Expense.js";
+import Integration from "./Integration.js";
+import Notification from "./Notification.js";
+import Payment from "./Payment.js";
+import Report from "./Report.js";
+import Role from "./Role.js";
+import Setting from "./Setting.js";
+import Subscription from "./Subscription.js";
+import UploadedReceipt from "./UploadedReceipt.js";
+import User from "./User.js";
+
+export {
+  AuditLog,
+  Billing,
+  Expense,
+  Integration,
+  Notification,
+  Payment,
+  Report,
+  Role,
+  Setting,
+  Subscription,
+  UploadedReceipt,
+  User,
+};
