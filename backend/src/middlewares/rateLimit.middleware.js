09a9ee827940d05ff64a24f239f89e52e918c078
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-23 22:11:46 +0530

    Update rateLimit.middleware.js

--- backend/src/middlewares/rateLimit.middleware.js
+++ backend/src/middlewares/rateLimit.middleware.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit'
 
-export const limiter = rateLimit({
+export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-  message: { success: false, message: 'Too many requests, please try again later' },
+  max: 100, // limit each IP
+  message: { success: false, message: 'Too many requests, please try again later.' },
 })
