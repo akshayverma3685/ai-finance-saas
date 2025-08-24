8875a9288f7397b580310cd68d2fb556e91e5249
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-24 21:48:00 +0530

    Delete frontend/src/components/layout directory

--- frontend/src/components/layout/index.jsx
+++
@@ -1,6 +0,0 @@
-// components/layout/index.js
-export { default as Navbar } from "./Navbar";
-export { default as Sidebar } from "./Sidebar";
-export { default as Footer } from "./Footer";
-export { default as Topbar } from "./Topbar";
-export { default as Layout } from "./Layout";
