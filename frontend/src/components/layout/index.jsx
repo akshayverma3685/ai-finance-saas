ee52ac0b433da5f84881ff5dfdde83b89a092411
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-24 20:37:23 +0530

    Update index.jsx

--- frontend/src/components/layout/index.jsx
+++ frontend/src/components/layout/index.jsx
@@ -2,3 +2,5 @@
 export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Footer } from "./Footer";
+export { default as Topbar } from "./Topbar";
+export { default as Layout } from "./Layout";
