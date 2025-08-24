d0409b1a799cbb7845646ce7561ef08d76765194
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-24 19:36:26 +0530

    Create Layout.js

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,14 @@
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        <main className="p-4">{children}</main>
+      </div>
+    </div>
+  );
+}
