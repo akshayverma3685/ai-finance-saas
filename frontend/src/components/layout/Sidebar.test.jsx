7478a064bc745fb68bb959cd3d37ecc65a2fe493
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-24 20:34:18 +0530

    Delete frontend/src/components/layout/Sidebar.test.jsx

--- frontend/src/components/layout/Sidebar.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Sidebar from "./Sidebar";
-
-describe("Sidebar Component", () => {
-  it("renders Sidebar", () => {
-    render(<Sidebar />);
-    expect(screen.getByText(/sidebar/i)).toBeInTheDocument();
-  });
-});
