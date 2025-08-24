4571313a465d62cf23cbd532e0e9ccac56a36147
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-24 23:20:14 +0530

    Delete frontend directory

--- frontend/src/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/utils/formatCurrency.js
-export default function formatCurrency(amount, currency = "USD") {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency,
-  }).format(amount);
-}
