58acc2c40666592b6e18eabf2285d8d549ef3f72
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 17:00:10 +0530

    Update metrics.js

--- backend/src/utils/metrics.js
+++ backend/src/utils/metrics.js
@@ -1,1 +1,22 @@
+const client = require('prom-client');
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const requestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.1, 0.5, 1, 2, 5]
+});
+
+const incrementCounter = (counterName, labels = {}) => {
+  const counter = new client.Counter({ name: counterName, help: counterName });
+  counter.inc(labels);
+};
+
+module.exports = {
+  client,
+  requestDuration,
+  incrementCounter
+};
