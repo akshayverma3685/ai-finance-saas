afe2da090dca82d3d2a646bc9c6a107158bc9bfa
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 16:51:24 +0530

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,9 +1,7 @@
-// src/pages/_app.js
 import "@/styles/globals.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  // Optional: remove React strict warnings in dev OR add global setup here
   useEffect(() => {
     console.log("App mounted ✅");
   }, []);
