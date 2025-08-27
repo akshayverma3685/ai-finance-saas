823739d83ac47cab8a3b0230ed4dec331872d181
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 16:24:12 +0530

    Update notification.service.js

--- backend/src/notifications/notification.service.js
+++ backend/src/notifications/notification.service.js
@@ -1,17 +1,14 @@
 const Notification = require("../models/Notification");
 
-// Create a new notification
 exports.create = async (data) => {
   const notification = new Notification(data);
   return await notification.save();
 };
 
-// Find all notifications
 exports.findAll = async () => {
   return await Notification.find().sort({ createdAt: -1 });
 };
 
-// Mark as read
 exports.markAsRead = async (id) => {
   const notification = await Notification.findByIdAndUpdate(
     id,
